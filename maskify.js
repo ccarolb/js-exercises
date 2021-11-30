eecb1461c7240d15133e267db63327fba07f4bf6
Carolina Borges <carolinaborgescardoso@gmail.com>
2021-11-29 21:13:10 -0300

    refatoração de código

--- maskify.js
+++ maskify.js
@@ -1,10 +1,6 @@
 // return masked string
 function maskify(cc) {
-
-    //retorna uma nova string com os 4 últimos caracteres da original
-    let masking = cc.slice(-4)
-    //completa a string com o caractere do segundo parametro até atingir o tamanho designado no primeiro parametro
-    let masked = masking.padStart(cc.length, "#")
+    let masked = cc.slice(-4).padStart(cc.length, "#")
     return masked
 }
 
