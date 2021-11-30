e7ac04f226d96648417fe2956f72f9f095b75614
Carolina Borges <carolinaborgescardoso@gmail.com>
2021-11-29 21:10:40 -0300

    destrinchando os métodos slice e padStart

--- maskify.js
+++ maskify.js
@@ -1,6 +1,10 @@
 // return masked string
 function maskify(cc) {
-    let masked = cc.slice(-4).padStart(cc.length, "#")
+
+    //retorna uma nova string com os 4 últimos caracteres da original
+    let masking = cc.slice(-4)
+    //completa a string com o caractere do segundo parametro até atingir o tamanho designado no primeiro parametro
+    let masked = masking.padStart(cc.length, "#")
     return masked
 }
 
